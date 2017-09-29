7c489660da3dc07d82ff151a053141c30ccb22e8
Solomin Andrey <solomin.andrey@gmail.com>
2017-09-29 14:37:08 +0300

    Added first activity

---
+++ app/src/main/java/ru/solandme/cryptominerutil/CoinList.java
@@ -0,0 +1,13 @@
+package ru.solandme.cryptominerutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoinList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_list);
+    }
+}
